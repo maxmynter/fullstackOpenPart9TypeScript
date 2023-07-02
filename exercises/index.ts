022872cc0263f932a6d47931274a0dd6ec444c97
Max Mynter <maxmynter@me.com>
2023-07-02 16:53:31 +0200

    Ex. 9.5

--- exercises/index.ts
+++ exercises/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import { calculateBMI } from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  if (isNaN(height) || isNaN(weight)) {
+    res
+      .status(400)
+      .send({ error: "Malfromatted parameters. Need height and weight" });
+  } else {
+    const resultBMI = calculateBMI(height, weight);
+    res.send(resultBMI);
+  }
 });
 
 const PORT = 3004;
