4eda75e1355d5d978fa03bd2ed5429039bfb9e91
Max Mynter <maxmynter@me.com>
2023-07-02 12:33:04 +0200

    TS files and cmd commands for multiplier and calculator

---
+++ calculator.ts
@@ -0,0 +1,24 @@
+type Operation = "multiply" | "add" | "divide";
+
+const calculator = (a: number, b: number, op: Operation) => {
+  switch (op) {
+    case "multiply":
+      return a * b;
+    case "add":
+      return a + b;
+    case "divide":
+      return a / b;
+    default:
+      throw new Error("Operation unkown. Use multiply, divide, or add.");
+  }
+};
+
+try {
+  console.log(calculator(1, 5, "divide"));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
