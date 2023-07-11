0858346d7a47a462b9de91febe7d8d369f46827f
Max Mynter <maxmynter@me.com>
2023-07-11 19:01:41 +0200

    Ex 9.15

--- newReactApp/fsopen9/src/App.tsx
+++ newReactApp/fsopen9/src/App.tsx
@@ -1,21 +1,60 @@
-interface HeaderProps {
-  name: string;
-}
+import { HeaderProps, coursePart } from "./types";
+import coursePartsData from "./data/data";
 
 const Header = (props: HeaderProps) => {
   return <h1>{props.name}</h1>;
 };
 
-interface coursePart {
-  name: string;
-  exerciseCount: number;
-}
+const Part = ({ coursePart }: { coursePart: coursePart }) => {
+  switch (coursePart.kind) {
+    case "basic":
+      return (
+        <div>
+          <h1 key={coursePart.name}>
+            {coursePart.name} {coursePart.exerciseCount}
+          </h1>
+          <text>{coursePart.description}</text>
+        </div>
+      );
+    case "group":
+      return (
+        <div>
+          <h1 key={coursePart.name}>
+            {coursePart.name} {coursePart.exerciseCount}
+          </h1>
+          <text> Group Projects {coursePart.groupProjectCount}</text>
+        </div>
+      );
+    case "background":
+      return (
+        <div>
+          <h1 key={coursePart.name}>
+            {coursePart.name} {coursePart.exerciseCount}
+          </h1>
+          <text>{coursePart.description}</text>
+          <text>Background Material: {coursePart.backgroundMaterial}</text>
+        </div>
+      );
+    case "special":
+      return (
+        <div>
+          <h1 key={coursePart.name}>
+            {coursePart.name} {coursePart.exerciseCount}
+          </h1>
+          <text>{coursePart.description}</text>
+          <text>Required Skills: {coursePart.requirements}</text>
+        </div>
+      );
+    default:
+      return <></>;
+  }
+};
 
 const Content = ({ content }: { content: coursePart[] }) => {
   const renderedContent = content.map((entry) => (
-    <p key={entry.name}>
-      {entry.name} {entry.exerciseCount}
-    </p>
+    <>
+      <Part coursePart={entry} />
+    </>
   ));
   return <div> {renderedContent}</div>;
 };
@@ -29,20 +68,7 @@ const Total = ({ content }: { content: coursePart[] }) => {
 };
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts: coursePart[] = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7,
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14,
-    },
-  ];
+  const courseParts: coursePart[] = coursePartsData;
 
   return (
     <div>
